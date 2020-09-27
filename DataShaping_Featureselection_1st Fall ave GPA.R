873e2dd1a3279a83eee64ddc2ede01a9d5db46ab
J-Sparks <68030345+J-Sparks@users.noreply.github.com>
2020-09-27 12:51:21 -0400

    Update DataShaping_Featureselection_1st Fall ave GPA.R

--- DataShaping_Featureselection_1st Fall ave GPA.R
+++ DataShaping_Featureselection_1st Fall ave GPA.R
@@ -11,21 +11,24 @@ upCRSCSE <- updated1CRSCSEData
 upCSE <- updated1CSEdata
 
 ## detect summer starter term
-upCRSCSE05 <- upCRSCSE$DEMO_TIME_FRAME
-as.factor(upCRSCSE05)
-s = str_sub(upCRSCSE05,5) # left to right 5th...
-upCRSCSE$DEMO_TIME_FRAME <- as.numeric(s) 
-glimpse(upCRSCSE)
+#upCRSCSE05 <- upCRSCSE$DEMO_TIME_FRAME
+#as.factor(upCRSCSE05)
+#s = str_sub(upCRSCSE05,5) # left to right 5th...
+#upCRSCSE$DEMO_TIME_FRAME <- as.numeric(s) 
+#glimpse(upCRSCSE) # found issue with someone had several fall terms
 
 #filter for 1st term courses
 upCRSCSEV1 <- upCRSCSE %>% 
   group_by(UNIV_ROW_ID) %>% 
   arrange(COURSE_NAME) %>% 
-  filter(DEMO_TIME_FRAME == min(DEMO_TIME_FRAME)) %>%  # choose for Fall term ==8 term courses
-  filter(STU_SECTN_CRED >= 3 & STU_SECTN_CRED <= 5) %>%  # filter credit hours between 3 to 5 hours
+  filter(DEMO_TIME_FRAME == 201208 | DEMO_TIME_FRAME == 201308 | DEMO_TIME_FRAME == 201408 | 
+           DEMO_TIME_FRAME == 201508 | DEMO_TIME_FRAME == 201608 | DEMO_TIME_FRAME == 201708 | 
+           DEMO_TIME_FRAME == 201808 | DEMO_TIME_FRAME == 201908  ) %>%  # choose for Fall term 
+  filter(DEMO_TIME_FRAME == min(DEMO_TIME_FRAME), .by_group=TRUE) %>% 
+  filter(STU_SECTN_CRED >= 3 & STU_SECTN_CRED <= 4) %>%  # filter credit hours between 3 to 4 hours,PHY2048C(5hrs omitted one term offered)
   mutate(Numcrs = row_number()) #max is the max number crs taken
 
-length(unique(upCRSCSEV1$COURSE_NAME)) #  392 courses
+length(unique(upCRSCSEV1$COURSE_NAME)) #  388 courses
 unique(upCRSCSEV1$Numcrs) #1 2 3 4 5 6 the max course are 6
 
 
@@ -62,10 +65,14 @@ colnames(merge3) <- c("UNIV_ROW_ID","CRSNAME1","CRSNAME2","CRSNAME3","CRSNAME4",
                       "CRScount1","CRScount2","CRScount3","CRScount4","CRScount5","CRScount6")
 
 
-
-
 CSE_wideCRS  <- merge(merge3, CSEdata, by="UNIV_ROW_ID", all.y=TRUE)
-glimpse(merge3)#""
 
+#CSE_wideCRS[-6,]
+#CSE_wideCRS[-39,]
+#CSE_wideCRS[-1342,]
+#CSE_wideCRS[-1446,]
+glimpse(CSE_wideCRS)
 
-write.csv(CSE_wideCRS,"wide_AllCSECRS_data.csv")
+glimpse(CSE_wideCRS)
+
+write.csv(CSE_wideCRS,"V1wide_AllCSECRS_data.csv")
